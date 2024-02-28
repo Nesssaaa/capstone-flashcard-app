38e307ec44a9fe530cb116c19e665c39a4209c77
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-28 10:22:47 +0100

    refactor CardList x to card

--- components/CardList/CardList.js
+++ components/CardList/CardList.js
@@ -3,9 +3,9 @@ import CardContainer from "../CardContainer/CardContainer";
 export default function CardList({ initialData }) {
   return (
     <>
-      {initialData.map((x) => (
-        <div key={x.id}>
-          <CardContainer question={x.question} answer={x.answer} />
+      {initialData.map((card) => (
+        <div key={card.id}>
+          <CardContainer question={card.question} answer={card.answer} />
         </div>
       ))}
     </>
