6680995a7b6c0a71f7ca6f0324c4592f97d247ac
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-03-04 10:49:16 +0100

    add styling

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-// export function calculateFontSize(text) {
-//   const wordCount = text.trim().split(/\s+/).length;
-//   const maxFontSize = 40;
-//   const minFontSize = 8;
-//   return Math.max(minFontSize, maxFontSize - wordCount / 4);
-// }
+export function calculateFontSize(text) {
+  const wordCount = text.trim().split(/\s+/).length;
+  const maxFontSize = 40;
+  const minFontSize = 8;
+  return Math.max(minFontSize, maxFontSize - wordCount / 4);
+}
