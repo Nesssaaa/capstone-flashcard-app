c79e1954f4d1638bf0b431efc89489df3099b245
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-03-07 10:10:07 +0100

    change App-Name and Header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,5 @@
 import { StyledHeader } from "./Header.styled.js";
 
 export default function Header() {
-  return <StyledHeader>Flashcard App</StyledHeader>;
+  return <StyledHeader>SchlauFuchs 🦊</StyledHeader>;
 }
--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.div`
   display: flex;
-  background-color: var(--color-background-3);
+  background-color: #ffffff33;
   padding: 1rem;
   margin-bottom: 1.5rem;
   flex-direction: row;
   justify-content: center;
-  box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.5);
-  font-size: 1.5rem;
+  /* box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.5); */
+  font-size: 2.5rem;
+  color: #ba7309;
+  font-weight: bold;
+  border-radius: 0 0 30px 30px;
 `;
