e29b6fa3c14e7c65bf1f5b05e8fe7d1c99e40448
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-03-12 10:42:35 +0100

    add Menu-Component and Basic-Styling

---
+++ components/Menu/Menu.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: var(--color-background-1);
+  height: 90%;
+  width: 60%;
+`;
+
+export const MenuItems = styled.li`
+  list-style: none;
+  font-size: 1.5rem;
+`;
