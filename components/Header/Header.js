59e3b79f77af0bde3da547d71907b29e62c75378
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-03-13 15:22:27 +0100

    remove more comments

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,24 +11,3 @@ export default function Header() {
     </StyledHeader>
   );
 }
-
-// import MenuComponent from "../MenuComponent/MenuComponent.js";
-// import { useState } from "react";
-// import { StyledHeader, IconWrapper, StyledSpan } from "./Header.styled.js";
-
-// export default function Header() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   function toggleMenu() {
-//     setIsOpen((prevOpen) => !prevOpen);
-//   }
-
-//   return (
-//     <StyledHeader>
-//       SchlauFuchs <IconWrapper> 🦊</IconWrapper>
-//       <StyledSpan>
-//         <MenuComponent toggleMenu={toggleMenu} isOpen={isOpen} />
-//       </StyledSpan>
-//     </StyledHeader>
-//   );
-// }
