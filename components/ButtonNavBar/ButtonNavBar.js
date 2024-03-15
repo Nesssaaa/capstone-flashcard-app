0fb3dc635e765cfb2f3487b6dd0046eb706be006
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-03-15 12:17:34 +0100

    delete edit and delete-button from Button-NavBar-Component

--- components/ButtonNavBar/ButtonNavBar.js
+++ components/ButtonNavBar/ButtonNavBar.js
@@ -1,51 +1,11 @@
 import CardButton from "../CardButton/CardButton";
-import { useRouter } from "next/router";
 import { IconWrapper, StyledButtonNavBar } from "./ButtonNavBar.styled";
-import { MdDeleteForever, MdEdit, MdDone } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { FaRepeat } from "react-icons/fa6";
-import { confirmAlert } from "react-confirm-alert";
-import "react-confirm-alert/src/react-confirm-alert.css";
 
-export default function ButtonNavBar({ id, deleteCard, onToggle, isMastered }) {
-  const router = useRouter();
-
-  function onEdit() {
-    router.push(`/cards/${id}/edit`);
-  }
-
-  function handleDelete() {
-    confirmAlert({
-      title: "Karte löschen?",
-      message: "Möchtest du diese Karte wirklich löschen?",
-      buttons: [
-        {
-          label: "Ja, bitte.",
-          onClick: () => {
-            deleteCard(id);
-          },
-        },
-        {
-          label: "Nein, danke.",
-        },
-      ],
-    });
-  }
-
+export default function ButtonNavBar({ id, onToggle, isMastered }) {
   return (
     <StyledButtonNavBar>
-      <CardButton onClick={onEdit}>
-        <IconWrapper>
-          <MdEdit />
-        </IconWrapper>
-      </CardButton>
-
-      <CardButton onClick={handleDelete}>
-        <IconWrapper>
-          <MdDeleteForever />
-        </IconWrapper>
-      </CardButton>
-
       <CardButton onClick={() => onToggle(id)}>
         <IconWrapper>{isMastered ? <FaRepeat /> : <FaCheck />}</IconWrapper>
       </CardButton>
