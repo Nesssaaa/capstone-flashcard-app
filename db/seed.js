12368bd7e294a963e5da2614fa11d5b09aedd385
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-03-16 15:26:41 +0100

    seed empty db with initialData

---
+++ db/seed.js
@@ -0,0 +1,20 @@
+import Card from "@/db/models/Card";
+import Deck from "@/db/models/Deck";
+import initialCards from "../lib.data.json";
+import initialCollections from "../lib.collections.json";
+import { cardToDb, collectionToDb } from "./utils";
+
+export async function seedDb() {
+  let cards = await Card.find();
+  if (cards.length > 0) return cards;
+
+  for (const card of initialCards) {
+    await Card.create(cardToDb(card));
+  }
+
+  for (const deck of initialCollections) {
+    await Deck.create(collectionToDb(deck));
+  }
+
+  return initialCards;
+}
