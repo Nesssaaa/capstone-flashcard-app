896dc68608a58d98c0a98113259ad7c14606b20c
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-03-17 22:27:12 +0100

    add API-route for collection

---
+++ pages/api/collections/index.js
@@ -0,0 +1,27 @@
+import dbConnect from "@/db/connect.js";
+import { collectionToDb, dbToCollection } from "@/db/utils";
+import Deck from "@/db/models/Deck";
+import { seedDb } from "@/db/seed";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    let collections = await Deck.find();
+
+    return response
+      .status(200)
+      .json(collections.map((dbCollection) => dbToCollection(dbCollection)));
+  }
+
+  if (request.method === "POST") {
+    try {
+      const newCollection = await Deck.create(collectionToDb(request.body));
+
+      return response.status(201).json(dbToCollection(newCollection));
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
