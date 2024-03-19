6cae2bc84e5481728a3efb166c3926f8ae613447
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-03-19 11:39:15 +0100

    add deleteMany to delete all associated cards

--- pages/api/collections/[id].js
+++ pages/api/collections/[id].js
@@ -1,6 +1,21 @@
 import dbConnect from "@/db/connect.js";
 import { collectionToDb, dbToCollection } from "@/db/utils";
 import Deck from "@/db/models/Deck";
+import Card from "@/db/models/Card";
+
+// export default async function handler(request, response) {
+//   await dbConnect();
+//   const { id } = request.query;
+
+//   if (request.method === "DELETE") {
+//     const deleteDeck = await Deck.findByIdAndDelete(id);
+//     await Card.deleteMany({ decks: id });
+//     await Deck.findByIdAndDelete(id);
+//     return response.status(200).json({
+//       status: `Collection ${id} including all associatedcards successfully deleted.`,
+//     });
+//   }
+// }
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -8,8 +23,11 @@ export default async function handler(request, response) {
 
   if (request.method === "DELETE") {
     await Deck.findByIdAndDelete(id);
-    return response
-      .status(200)
-      .json({ status: `Collection ${id} successfully deleted.` });
+    await Card.deleteMany({ deck: id });
+
+    return response.status(200).json({
+      status: `Collection ${id} including all associated cards successfully deleted.`,
+    });
   }
+  mutate();
 }
