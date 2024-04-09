b6737a8d066127c33484d764f58a0d5183c3d2bd
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-04-09 22:23:35 +0200

    finished refactoring

--- components/CollectionNavBar/CollectionNavBar.js
+++ components/CollectionNavBar/CollectionNavBar.js
@@ -1,34 +1,21 @@
-import {
-  StyledNavigation,
-  StyledList,
-  StyledListItem,
-  StyledLink,
-  IconWrapper,
-  Space,
-} from "./CollectionNavBar.styled";
-
 import { BsCollectionFill } from "react-icons/bs";
 import { RiArchive2Fill } from "react-icons/ri";
 import { MdOutlineAddCircleOutline, MdDone } from "react-icons/md";
 import NavBarButton from "../Navigation/NavBarButton";
+import NavBar from "../Navigation/NavBar";
 
 export default function CollectionNavbar({ id }) {
   return (
-    <>
-      <Space />
-      <StyledNavigation>
-        <StyledList>
-          <NavBarButton href="/collections" icon={<BsCollectionFill />} />
-          <NavBarButton
-            href={`/create?collection=${id}`}
-            icon={<MdOutlineAddCircleOutline />}
-          />
-          <NavBarButton
-            href={`/collections/${id}?archive=true`}
-            icon={<RiArchive2Fill />}
-          />
-        </StyledList>
-      </StyledNavigation>
-    </>
+    <NavBar>
+      <NavBarButton href="/collections" icon={<BsCollectionFill />} />
+      <NavBarButton
+        href={`/create?collection=${id}`}
+        icon={<MdOutlineAddCircleOutline />}
+      />
+      <NavBarButton
+        href={`/collections/${id}?archive=true`}
+        icon={<RiArchive2Fill />}
+      />
+    </NavBar>
   );
 }
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,31 +1,15 @@
-import { useRouter } from "next/router";
-import {
-  IconWrapper,
-  StyledLink,
-  StyledList,
-  StyledListItem,
-  StyledNavigation,
-  Space,
-} from "./Navigation.styled";
 import { MdOutlineAddCircleOutline } from "react-icons/md";
 import { BsCollectionFill } from "react-icons/bs";
 import { RiArchive2Fill } from "react-icons/ri";
 import NavBarButton from "./NavBarButton";
+import NavBar from "./NavBar";
 
 export default function Navigation() {
-  const router = useRouter();
   return (
-    <>
-      <Space />
-      <StyledNavigation>
-        <StyledList>
-          <NavBarButton href="/collections" icon={<BsCollectionFill />} />
-
-          <NavBarButton href="/create" icon={<MdOutlineAddCircleOutline />} />
-
-          <NavBarButton href="/archive" icon={<RiArchive2Fill />} />
-        </StyledList>
-      </StyledNavigation>
-    </>
+    <NavBar>
+      <NavBarButton href="/collections" icon={<BsCollectionFill />} />
+      <NavBarButton href="/create" icon={<MdOutlineAddCircleOutline />} />
+      <NavBarButton href="/archive" icon={<RiArchive2Fill />} />
+    </NavBar>
   );
 }
