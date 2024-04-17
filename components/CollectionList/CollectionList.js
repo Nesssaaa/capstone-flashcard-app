37505717e30b938dcbf89857ee7d9a90439ccf46
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-04-17 13:18:55 +0200

    sort collections by timestamp

--- components/CollectionList/CollectionList.js
+++ components/CollectionList/CollectionList.js
@@ -11,28 +11,35 @@ export default function CollectionList({
   toggleCardDirection,
   editCollection,
 }) {
+  console.log(collections);
   return (
     <>
       {collections &&
-        collections.map((collection) => {
-          const filteredCards = cards.filter(
-            (card) => card.collection === collection.id
-          );
-          return (
-            <StyledListContainer key={collection.id}>
-              <CollectionContainer
-                collection={collection}
-                editCollection={editCollection}
-                deleteCollection={deleteCollection}
-                cards={filteredCards}
-                archive={archive}
-                editCard={editCard}
-                resetCard={resetCard}
-                toggleCardDirection={toggleCardDirection}
-              />
-            </StyledListContainer>
-          );
-        })}
+        collections
+          .sort((c1, c2) => {
+            if (c1.timestamp > c2.timestamp) return -1;
+            if (c1.timestamp < c2.timestamp) return 1;
+            return 0;
+          })
+          .map((collection) => {
+            const filteredCards = cards.filter(
+              (card) => card.collection === collection.id
+            );
+            return (
+              <StyledListContainer key={collection.id}>
+                <CollectionContainer
+                  collection={collection}
+                  editCollection={editCollection}
+                  deleteCollection={deleteCollection}
+                  cards={filteredCards}
+                  archive={archive}
+                  editCard={editCard}
+                  resetCard={resetCard}
+                  toggleCardDirection={toggleCardDirection}
+                />
+              </StyledListContainer>
+            );
+          })}
     </>
   );
 }
