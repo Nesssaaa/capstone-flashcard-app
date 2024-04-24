44a785997813c80f71afba8f72698041495d72b2
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-04-24 12:32:34 +0200

    add callbackUrl to signIn

--- components/LoginButton/LoginButton.js
+++ components/LoginButton/LoginButton.js
@@ -29,7 +29,7 @@ export default function LoginButton() {
       <>
         <StyledButton
           aria-label="Hier kannst du dich anmelden"
-          onClick={() => signIn()}
+          onClick={() => signIn(null, { callbackUrl: "/collections" })}
         >
           Anmelden
         </StyledButton>
