0a5aa06f15decd9abb0dc4cb21898bb60b532d47
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-04-26 11:37:30 +0200

    style toggleButton and add aria-label

--- pages/allCards.js
+++ pages/allCards.js
@@ -1,9 +1,10 @@
 import CardList from "../components/CardList/CardList";
 import Navigation from "../components/Navigation/Navigation";
-import { useSession } from "next-auth/react";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import useSWR from "swr";
 import { useState } from "react";
+import styled from "styled-components";
+import { SlDirections } from "react-icons/sl";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
@@ -30,11 +31,17 @@ export default function AllCardsPage({
   return (
     <div>
       <SearchBar handleSearch={handleSearch} />
-      <button onClick={toggleReversedDirection}>
+      <StyledToggleDirection
+        aria-label="Kartenrichtung aller Karten umschalten"
+        onClick={toggleReversedDirection}
+      >
+        {" "}
+        <SlDirections />
+        &nbsp;&nbsp;
         {reversedDirection
-          ? "Drehe alle Karten wieder um und zeige die Vorderseiten"
-          : "Kartenrichtung umschalten und alle Rückseiten anzeigen"}
-      </button>
+          ? "Vorderseiten aller Karten anzeigen"
+          : "Rückseiten aller Karten anzeigen"}
+      </StyledToggleDirection>
       <CardList
         cards={cards}
         deleteCard={deleteCard}
@@ -48,3 +55,17 @@ export default function AllCardsPage({
     </div>
   );
 }
+
+export const StyledToggleDirection = styled.button`
+  width: 60%;
+  font-size: 1.1rem;
+  padding: 5px 5px;
+  border-radius: 0.9rem;
+  border: 1px solid var(--color-border-3);
+  margin-bottom: 2rem;
+  margin-left: 20%;
+  margin-right: 5%;
+  text-align: center;
+  background: var(--color-fox);
+  color: var(--color-font-2);
+`;
