4d712a5137673bc319adf12c7bf1fdc3f76f3e70
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-05-06 13:10:19 +0200

    add Wrapper and collectio[id].styled.js

---
+++ components/collections[id].styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+export const StyledContainer = styled.div`
+  text-align: center;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: var(--color-fox);
+`;
+
+export const Wrapper = styled.div`
+  margin: 0;
+  margin-bottom: 6rem;
+`;
